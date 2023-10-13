f18eb9fcd3b4c1f56a2587e8687d8d484bf6a1c7
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-13 15:51:58 -0300

    feat: add user domain

---
+++ src/users/entities/user.ts
@@ -0,0 +1,33 @@
+import { Email, Name, Password, type UserData } from '@/users/entities'
+import { InvalidNameError, InvalidEmailError, InvalidPasswordError } from '@/users/entities/errors'
+
+export class User {
+  public readonly name: Name
+  public readonly email: Email
+  public readonly password: Password
+
+  private constructor (name: Name, email: Email, password: Password) {
+    this.name = name
+    this.email = email
+    this.password = password
+  }
+
+  static create (userData: UserData): User | InvalidNameError | InvalidEmailError | InvalidPasswordError {
+    const nameOrError = Name.create(userData.name)
+    if (nameOrError instanceof InvalidNameError) {
+      return nameOrError
+    }
+
+    const emailOrError = Email.create(userData.email)
+    if (emailOrError instanceof InvalidEmailError) {
+      return emailOrError
+    }
+
+    const passwordOrError = Password.create(userData.password)
+    if (passwordOrError instanceof InvalidPasswordError) {
+      return passwordOrError
+    }
+
+    return new User(nameOrError, emailOrError, passwordOrError)
+  }
+}
