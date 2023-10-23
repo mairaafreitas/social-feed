96ec1aa3b6d35515ccb0601938030a8901e8ddfa
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-23 08:12:18 -0300

    feat: add mongo helper to connect to mongodb

---
+++ src/shared/infra/mongodb-helper/mongo-helper.ts
@@ -0,0 +1,21 @@
+import { type Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri)
+  },
+
+  async disconnect (): Promise<void> {
+    this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  async clearCollection (name: string): Promise<void> {
+    this.client.db().collection(name).deleteMany({})
+  }
+}
