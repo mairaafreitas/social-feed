e92c3d7d2250285d42c11842a31da183176d764c
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-27 09:37:07 -0300

    test: fix test user entity

--- test/users/entities/user.spec.ts
+++ test/users/entities/user.spec.ts
@@ -1,4 +1,5 @@
 import { User } from '@/users/entities'
+import { type InvalidNameError, type InvalidEmailError, type InvalidPasswordError } from '@/users/entities/errors'
 
 describe('User domain entity', () => {
   test('should create user with valid data', () => {
@@ -12,21 +13,21 @@ describe('User domain entity', () => {
   })
   test('should not create user with invalid email address', () => {
     const invalidEmail = 'invalid_email'
-    const error = User.create({ name: 'Any Name', email: invalidEmail, password: 'Xpassword1*' }) as Error
+    const error = User.create({ name: 'Any Name', email: invalidEmail, password: 'Xpassword1*' }) as InvalidEmailError
     expect(error.name).toEqual('InvalidEmailError')
     expect(error.message).toEqual(`Invalid email: ${invalidEmail}.`)
   })
 
   test('should not create user with invalid name (too few characters)', () => {
     const invalidName = 'O'
-    const error = User.create({ name: invalidName, email: 'any@email.com', password: 'Xpassword1*' }) as Error
+    const error = User.create({ name: invalidName, email: 'any@email.com', password: 'Xpassword1*' }) as InvalidNameError
     expect(error.name).toEqual('InvalidNameError')
     expect(error.message).toEqual(`Invalid name: ${invalidName}.`)
   })
 
   test('should not create user with invalid password (no special character)', () => {
     const invalidPassword = 'Xpassword1'
-    const error = User.create({ name: 'Any Name', email: 'any@email.com', password: invalidPassword }) as Error
+    const error = User.create({ name: 'Any Name', email: 'any@email.com', password: invalidPassword }) as InvalidPasswordError
     expect(error.name).toEqual('InvalidPasswordError')
     expect(error.message).toEqual('Invalid password. Check if you password has at least 8 characters long, upper and lower case, has number and at special character.')
   })
