35b2abe3b3cc50853e54034176a7d2288c7d92a1
mairaafreitas <maira.oliveirafreitas@gmail.com>
2023-10-27 09:38:25 -0300

    refactor: register user

--- src/users/usecases/register-user/register-user.ts
+++ src/users/usecases/register-user/register-user.ts
@@ -1,26 +1,29 @@
-import { type User, type UserData } from '@/users/entities'
-import { type UseCase } from '@/users/usecases/ports'
+import { User, type UserData } from '@/users/entities'
+import { InvalidNameError, InvalidEmailError, InvalidPasswordError } from '@/users/entities/errors'
+import { type RegisterUserUseCase } from '@/users/usecases/ports'
 import { type UserRepository } from '@/users/usecases/register-user/ports'
 
-export class RegisterUser implements UseCase {
+export class RegisterUser implements RegisterUserUseCase {
   private readonly userRepository: UserRepository
 
   constructor (userRepository: UserRepository) {
     this.userRepository = userRepository
   }
 
-  public async perform (request: User): Promise<UserData | undefined> {
-    try {
-      const name = request.name?.value
-      const email = request.email?.value
-      const password = request.password?.value
-      const userData = { name, email, password }
-      if (!(await this.userRepository.exists(userData))) {
-        await this.userRepository.add(userData)
-      }
-      return userData
-    } catch (error) {
-      console.log(error)
+  public async perform (request: UserData): Promise<InvalidNameError | InvalidEmailError | InvalidPasswordError | UserData > {
+    const user: User = User.create(request) as User
+    if (user instanceof InvalidNameError || user instanceof InvalidEmailError || user instanceof InvalidPasswordError) {
+      return user
     }
+
+    const name = user.name.value
+    const email = user.email.value
+    const password = user.password.value
+    const userData: UserData = { name, email, password }
+
+    if (await this.userRepository.exists(userData) === false) {
+      await this.userRepository.add(userData)
+    }
+    return { name: user.name.value, email: user.email.value, password: user.password.value }
   }
 }
